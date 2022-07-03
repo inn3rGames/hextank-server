d76b0100fec1bf731b73b34f289c3a23e567dd05
inn3rGames <hi@inn3r.com>
2022-07-03 14:02:32 +0300

    Add logic

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -1,7 +1,10 @@
-import { Schema, Context, type } from "@colyseus/schema";
+import { MapSchema, Schema, type } from "@colyseus/schema";
+
+export class Player extends Schema {
+  @type("number") x: number;
+  @type("number") z: number;
+}
 
 export class MyRoomState extends Schema {
-
-  @type("string") mySynchronizedProperty: string = "Hello world";
-
+  @type({ map: Player }) players = new MapSchema<Player>();
 }
