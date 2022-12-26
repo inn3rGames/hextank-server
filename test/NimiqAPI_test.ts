09d6d8634c0b63b0d681bf1f1cb191ed78e9be2a
inn3rGames <hi@inn3r.com>
2022-12-26 23:24:25 +0200

    Add test for .env file

--- test/NimiqAPI_test.ts
+++ test/NimiqAPI_test.ts
@@ -17,4 +17,18 @@ describe("Testing Nimiq logic", () => {
     it("Test network set properly", () => {
         assert.strictEqual("TEST" === process.env.NIMIQ_NETWORK_TYPE, true);
     });
+
+    it("The prize per killed HexTank is maximum 90% of the entry fee", () => {
+        const entryFee = parseInt(process.env.NIMIQ_LUNA_ENTRY_FEE as string);
+        const transactionFee = parseInt(
+            process.env.NIMIQ_LUNA_TRANSACTION_FEE as string
+        );
+        const prize = parseInt(process.env.NIMIQ_LUNA_PRIZE as string);
+
+        const totalPrize = 5 * prize + 7 * transactionFee;
+        const totalEntryFee = entryFee + 7 * transactionFee;
+        const percent = totalPrize / totalEntryFee;
+
+        assert.strictEqual(percent <= 0.9, true);
+    });
 });
