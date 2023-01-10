5e2aa600209e1b2ad29dc294fa8532c71e488db8
inn3rGames <hi@inn3r.com>
2023-01-10 06:54:06 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
         {
             name: "hextank-server",
             script: "./index.js",
+            node_args: ["--max-old-space-size=2048"],
             env: {
                 NODE_ENV: "production",
             },
